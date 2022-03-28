fa705620b027a43de7de18c217e0693b80968e2e
heromo <hero@asterisk-works.jp>
2022-03-28 17:16:41 +0900

    refactoring

--- test/snapshot-plugin.ts
+++ test/snapshot-plugin.ts
@@ -1,12 +1,17 @@
 // see https://github.com/facebook/jest/blob/main/packages/pretty-format/README.md#serialize
-module.exports = {
-  test(val: any) {
-    return typeof val === 'string' && /[a-z0-9]{64}.zip/.test(val);
-  },
-  serialize(val: string, config: object, indentation: string, depth: number, refs: any[]) {
-    if (refs[depth - 1].S3Key === val) {
-      return '"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.zip"';
-    }
-    return val;
-  },
-};
+export function test(val: any) {
+  return typeof val === 'string' && /[a-z0-9]{64}.zip/.test(val);
+}
+
+export function serialize(
+  val: string,
+  _config: object,
+  _indentation: string,
+  depth: number,
+  refs: any[],
+) {
+  if (refs[depth - 1].S3Key === val) {
+    return '"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.zip"';
+  }
+  return val;
+}
