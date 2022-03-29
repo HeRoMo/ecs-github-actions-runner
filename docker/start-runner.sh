a057dd8d10e2ab2e815ec9f17ce7572fc3bb17d0
heromo <hero@asterisk-works.jp>
2022-03-29 09:55:11 +0900

    disable auto upgrade

--- docker/start-runner.sh
+++ docker/start-runner.sh
@@ -2,5 +2,5 @@
 
 ## Start Github Actions Runner
 RUNNER_NAME="$(hostname)-$(uname -m)"
-./config.sh --unattended --replace --name ${RUNNER_NAME} --url ${REPOSITORY_URL} --token ${TOKEN}
+./config.sh --unattended --replace --disableupdate --name ${RUNNER_NAME} --url ${REPOSITORY_URL} --token ${TOKEN}
 ./run.sh
