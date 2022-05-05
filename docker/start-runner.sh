384ad6ad271571eabb1e01c2e2eb7878a1a9bf75
HeRoMo <hero@asterisk-works.jp>
2022-05-05 10:23:49 +0900

    Improve runner image (#70)

--- docker/start-runner.sh
+++ docker/start-runner.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 ## Start Github Actions Runner
-RUNNER_NAME="$(hostname)-$(uname -m)"
-./config.sh --unattended --replace --disableupdate --name ${RUNNER_NAME} --url ${REPOSITORY_URL} --token ${TOKEN}
+if [[ 'yes' == $EPHEMERAL ]]; then
+  ephemeral='--ephemeral'
+fi
+if [[ -n $LABELS ]]; then
+  labels="--labels ${LABELS}"
+fi
+echo '----- SETTINGS -----------------------------------------'
+echo "REPOSITORY_URL: ${REPOSITORY_URL}"
+echo "ephemeral: ${ephemeral}"
+echo "labels: ${labels}"
+echo '--------------------------------------------------------'
+./config.sh --unattended --replace --disableupdate --url ${REPOSITORY_URL} --token ${TOKEN} ${ephemeral} ${labels}
 ./run.sh
